9b1c56b2f9b859af99f469731fbb718c8e5082df
Dave Tarrant <davetaz@theodi.org>
2016-07-29 15:02:40 +0100

    Added hover reverse

--- action.js
+++ action.js
@@ -4,28 +4,24 @@ $( document ).ready(function() {
 
 var filter_on = false;
 function processListeners() {
-	$("div.hex2").click(
+	$("div.hexagon").click(
 		function() {
-			current = $(this).css("opacity");
+			current = $(this).parent().css("opacity");
 			if (current == 1 && filter_on) {
 				$('.hex2').css("opacity",1);
 				$('.course_filter').css("display","block");
 				filter_on = false;
 			} else {
 				$('.hex2').css("opacity",0.5);
-				$(this).css("opacity",1);
+				$(this).parent().css("opacity",1);
 				$('.course_filter').css("display","none");
 			
-				courses_all = $(this).find('h4').attr("courses");
+				courses_all = this.className
 				courses = courses_all.split(' ');
 				for (i = 0; i < courses.length; i++) { 
     				$('#'+courses[i]).css("display","block");
 				}
-				elearning_all = $(this).find('h4').attr("elearning");
-				elearning = elearning_all.split(' ');
-				for (i = 0; i < elearning.length; i++) { 
-    				$('#'+elearning[i]).css("display","block");
-				}
+				
 				filter_on = true;
 			}
     	}
@@ -37,6 +33,17 @@ function processListeners() {
 			//console.log(type);
    			$('.hex2').css("opacity",0.5);
    			$('.'+type).parent().css("opacity",1);
+    	}, function() {
+      		$('.hex2').css("opacity",1);
+	  	}
+	);
+	$("div.course_filter").hover(
+		function() {
+			var id = this.id;
+			console.log(id);
+			//console.log(type);
+   			$('.hex2').css("opacity",0.5);
+   			$('.'+id).parent().css("opacity",1);
     	}, function() {
       		$('.hex2').css("opacity",1);
 	  	}
